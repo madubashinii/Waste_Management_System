f29801b5f3bea941f658a66ebe33f82e0053fb99
madubashinii <nilakshimadubashini920@gmail.com>
2025-10-15 00:31:22 +0530

    refactor: update App.js with Collector routes and navigation

--- eco-collect-frontend/src/App.jsx
+++ eco-collect-frontend/src/App.jsx
@@ -1,15 +1,41 @@
-import './App.css'
-import Test from "./Test";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Home from './pages/Home';
+// import SignUp from './pages/SignUp';
+// import SignIn from './pages/SignIn';
+import CollectorRoutesWrapper from './pages/collector/CollectorRoutesWrapper';
+import './App.css';
 
 function App() {
     return (
-        <>
-            <div style={{textAlign: "center", marginTop: "50px"}}>
-                <h1>React + Spring Boot Test</h1>
-                <Test/>
+        <Router>
+            <div className="min-h-screen flex flex-col">
+                {/*<Header />*/}
+
+                <div className="flex-1">
+                    <Routes>
+                        {/* Public Pages */}
+                        {/*<Route path="/" element={<Home />} />*/}
+                        {/*<Route path="/signup" element={<SignUp />} />*/}
+                        {/*<Route path="/signin" element={<SignIn />} />*/}
+
+                        {/* Collector Pages */}
+                        <Route path="/collector/*" element={<CollectorRoutesWrapper />} />
+
+                        {/* Redirect unknown paths */}
+                        {/*<Route path="*" element={<Navigate to="/" replace />} />*/}
+                    </Routes>
+                </div>
+
+                {/*<Footer />*/}
             </div>
-        </>
-    )
+        </Router>
+    );
 }
-h
-export default App
+
+export default App;
+
+
+
+
