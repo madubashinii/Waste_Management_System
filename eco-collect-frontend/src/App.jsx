962f83373d49de1d578d0b6dada4a8b8ed7edaff
madubashinii <nilakshimadubashini920@gmail.com>
2025-10-14 15:50:36 +0530

    Replace old React frontend with Vite + React setup and add Tailwind CSS

---
+++ eco-collect-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Test from "./Test";
+
+function App() {
+    return (
+        <>
+            <div style={{textAlign: "center", marginTop: "50px"}}>
+                <h1>React + Spring Boot Test</h1>
+                <Test/>
+            </div>
+        </>
+    )
+}
+h
+export default App
